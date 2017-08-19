d4f5e4078c36e9b8a18f4d2e09ea37c31d3f3daf
J.K <K_940201@hotmail.com>
2017-08-19 16:19:35 -0700

    Initial Commit

---
+++ shyStudent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shyStudent
+//
+//  Created by Jaewon Kim on 2017-08-19.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
