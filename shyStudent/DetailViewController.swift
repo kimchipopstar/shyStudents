b3e12db3324e376f106e342d91f4aa12cec52a4b
J.K <K_940201@hotmail.com>
2017-08-22 23:52:48 -0700

    chat is not showing in the log

--- shyStudent/DetailViewController.swift
+++ shyStudent/DetailViewController.swift
@@ -15,7 +15,7 @@ import JSQMessagesViewController
 
 class DetailViewController: JSQMessagesViewController
 {
-
+    //MARK: properties
     var channelRef: DatabaseReference?
     var channel: Channel? {
         didSet{
@@ -24,22 +24,111 @@ class DetailViewController: JSQMessagesViewController
     }
     var messages = [JSQMessage]()
     
+    lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
+    lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
+    
+    private lazy var messageRef: DatabaseReference = self.channelRef!.child("messages")
+    private var newMessageRefHandle: DatabaseHandle?
+    
+    //MARK: view lifecycle
+    
 
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.senderId = Auth.auth().currentUser?.uid
+        
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+        
     }
     
+//    override func viewDidAppear(_ animated: Bool) {
+//        
+//        addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
+//        
+//        addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
+//        addMessage(withId: senderId, name: "Me", text: "I like to run!")
+//        
+//        finishReceivingMessage()
+//    }
+    
+    //MARK: collection view datasource and delegate
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
         return messages[indexPath.item]
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        
+        let itemRef = messageRef.childByAutoId()
+        let messageItem = [
+            "senderId": senderId!,
+            "senderName": senderDisplayName!,
+            "text": text!,
+            ]
+        
+        itemRef.setValue(messageItem)
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        
+        finishSendingMessage()
         //SENDING -> Firebase (under the child channel)
     }
-
-    // MARK: - Navigation
+    
+    // MARK: Firebase related methods
+    
+    
+    
+    
+    // MARK: UI and User Interaction
+    
+    private func setupOutgoingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    }
+    
+    private func setupIncomingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item]
+        if message.senderId == senderId {
+            return outgoingBubbleImageView
+        } else {
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    private func addMessage(withId id: String, name: String, text: String) {
+        if let message = JSQMessage(senderId: id, displayName: name, text: text) {
+            messages.append(message)
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            cell.textView?.textColor = UIColor.white
+        } else {
+            cell.textView?.textColor = UIColor.black
+        }
+        return cell
+    }
+    
+    // MARK: UITextViewDelegate methods
     
 
 
